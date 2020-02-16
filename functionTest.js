e22edbc20c4713a2556b9e6db374c2dceec8203a
Julie Berryhill <juliesellsnc@gmail.com>
2020-02-16 11:52:07 -0500

    random

---
+++ functionTest.js
@@ -0,0 +1,15 @@
+function count() {
+    for (var i = 1; i < 6; i++) {
+        if (i < 5) {
+            console.log(i + "...")
+        
+        }  else {
+            console.log(i + "!!!!")
+        }
+       
+
+    }
+
+
+}
+count()
