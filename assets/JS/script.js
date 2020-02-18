663c16e18090cfbf90d1ca10fb206d9ea0d8039a
ccrum292 <ccrum292@gmail.com>
2020-02-18 10:52:03 -0500

    finished code for contact page

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -150,28 +150,28 @@ function displayQuestionOneAtATime(){
 
 var devTeam = [
   {
-    name: "Adriana J Didden",
-    portfolio: "",
-    gitHub: "",
-    link: "",
+    name: 'Adriana J Didden',
+    portfolio: '',
+    gitHub: '',
+    link: '',
   },
   {
-    name: "Caleb Crum",
-    portfolio: "",
-    gitHub: "",
-    link: "",
+    name: 'Caleb Crum',
+    portfolio: 'https://ccrum292.github.io/Responsive_Design/',
+    gitHub: 'https://github.com/ccrum292',
+    link: 'https://www.linkedin.com/in/caleb-crum-220a7319b/',
   },
   {
     name: "Julie Berryhill",
-    portfolio: "",
-    gitHub: "",
-    link: "",
+    portfolio: '',
+    gitHub: '',
+    link: '',
   },
   {
     name: "Monica Gonzalez Pena",
-    portfolio: "",
-    gitHub: "",
-    link: "",
+    portfolio: '',
+    gitHub: '',
+    link: '',
   }
 ];
 
@@ -181,7 +181,7 @@ function produceContactDisplay(){
   var contactHeading = $("<div id='display-div' class='container'><h2 class='title has-text-centered sriracha'>Developer Contact Information</h2><div id='answer-columns' class='columns'></div></div>")
   $("#contact-body").append(contactHeading);
   for (var i=0; i<devTeam.length; i++){
-  var devColumns = $("<div class='column has-text-centered div-within-answer-div'><div class='column is-danger has-text-weight-bold has-text-white mali answer-button'>"+  +"</div></div>")
+  var devColumns = $("<div class='column has-text-centered div-within-answer-div'><div class='column is-danger has-text-weight-bold has-text-white mali contact-info'><h3 class='has-text-weight-bold has-text-white'>"+devTeam[i].name +"</h3><form style='display: inline' target='_blank' action="+devTeam[i].portfolio +" method='get'><button class='button is-danger has-text-white contact-button'>Portfolio</button></form><form style='display: inline' target='_blank' action="+devTeam[i].gitHub +" method='get'><button class='button is-danger has-text-white contact-button'><i class='fab fa-github'></i></button></form><form style='display: inline' target='_blank' action="+devTeam[i].link +" method='get'><button class='button is-danger has-text-white contact-button'><i class='fab fa-linkedin-in'></i></button></form></div></div>")
   $(".columns").append(devColumns);
   }
 }
