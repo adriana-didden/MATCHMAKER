00318726703dbe5358145b3b3fa87df51e3ea420
tantatinta <bluekasia@gmail.com>
2020-02-19 10:11:34 -0500

    update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,7 +12,7 @@ $(window).on("load", function () {
   if (!localStorage.getItem("getLocation")) {
     getLocation();
     localStorage.setItem("getLocation", "true");
-  }else{
+  } else {
     coordinates = localStorage.getItem("coordinates")
   }
 })
@@ -33,7 +33,7 @@ function showPosition(position) {
   var lon = position.coords.longitude;
   coordinates = lat + "," + lon;
   localStorage.setItem("coordinates", coordinates)
-  
+
 }
 
 // Navbar Hamburger 
@@ -41,7 +41,7 @@ function showPosition(position) {
 $(document).ready(function () {
 
   $(".navbar-burger").click(function () {
-  
+
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
 
@@ -65,22 +65,22 @@ var questionsArray = [
   {
     title: "What fits you best:",
     choices: ["Man looking for a woman", "Woman looking for a man", "Man looking for a man", "Woman looking for a woman"],
-    
+
   },
   {
     title: "What are your thoughts on children?",
     choices: ["Want someday", "Don't want", "Have and want more", "Have and don't want more"],
-    
+
   },
   {
     title: "What is your preferred style?",
     choices: ["Preppy", "Hipster", "Casual", "Trendy"],
-    
+
   },
   {
     title: "What is your preferred body type?",
     choices: ["Slender", "Big and beautiful", "About average", "Athletic and toned"],
-    
+
   },
   {
     title: "What song fits you best?",
@@ -132,7 +132,7 @@ function click(event) {
 
 
 function filter() {
-  
+
   var answer1 = answers[0];
   var answer2 = answers[1];
   var answer3 = answers[2];
@@ -161,14 +161,14 @@ function filter() {
   }
   else if (answer3 === "Hipster") {
     coatType = "wire,short";
-  } 
+  }
   else if (answer3 === "Casual") {
     coatType = "short";
-  } 
+  }
   else if (answer3 === "Trendy") {
     coatType = "long,short";
-  } 
-  
+  }
+
   //["Slender", "Big and beautiful" xlarge, "About average" large, "Athletic and toned" medium: small, medium, large, xlarge]
   if (answer4 === "Slender") {
     dogSize = "small";
@@ -191,20 +191,20 @@ function filter() {
     buildRequest(searchGender, goodWithChildren, coatType, dogSize, goodWithCats);
     // console.log(searchGender)
   }
-  
+
 }
 
 
 function buildRequest(searchGender, goodWithChildren, coatType, dogSize, goodWithCats) {
   console.log(coordinates)
-  var params = { type: "dog", location:coordinates, gender: searchGender, good_with_children: goodWithChildren, coat: coatType, size: dogSize, good_with_cats: goodWithCats}
+  var params = { type: "dog", location: coordinates, status: "adoptable", gender: searchGender, good_with_children: goodWithChildren, coat: coatType, size: dogSize, good_with_cats: goodWithCats }
   console.log(params)
-  if(coordinates){
+  if (coordinates) {
     params.location = coordinates
     console.log(params.location)
     params.distance = 100
   }
-  
+
   pf.animal.search(params)
 
     .then(function (response) {
@@ -216,29 +216,35 @@ function buildRequest(searchGender, goodWithChildren, coatType, dogSize, goodWit
       var i = 0;
       function displayMatch() {
 
-          function dogBreed() {
-            dogId = (response.data.animals[i].id);
-            var dogBreedPrimary = (response.data.animals[i].breeds.primary);
-            var dogBreedSecondary = (response.data.animals[i].breeds.secondary);
-            var dogBreedMixed = (response.data.animals[i].breeds.mixed);
-            if (dogBreedPrimary && dogBreedSecondary && dogBreedMixed) {
-              return "This cutiepie is a " + dogBreedPrimary + " and " + dogBreedSecondary + " mix";
-            }
-            if (dogBreedPrimary && !dogBreedSecondary && dogBreedMixed) {              
-              return "This cutiepie is mainly a " + dogBreedPrimary + " mix";
-            }
-            if (dogBreedPrimary && !dogBreedSecondary && !dogBreedMixed) {              
-              return "This cutiepie is a " + dogBreedPrimary;
-            }
-            if (!dogBreedPrimary && !dogBreedSecondary && !dogBreedMixed) {
-              return "";
-            }
+        function dogBreed() {
+          dogId = (response.data.animals[i].id);
+          var dogBreedPrimary = (response.data.animals[i].breeds.primary);
+          var dogBreedSecondary = (response.data.animals[i].breeds.secondary);
+          var dogBreedMixed = (response.data.animals[i].breeds.mixed);
+          if (dogBreedPrimary && dogBreedSecondary && dogBreedMixed) {
+            return "This cutiepie is a " + dogBreedPrimary + " and " + dogBreedSecondary + " mix";
           }
-        var description = response.data.animals[i].description
-        if(!description) description ="";
-
+          if (dogBreedPrimary && !dogBreedSecondary && dogBreedMixed) {
+            return "This cutiepie is mainly a " + dogBreedPrimary + " mix";
+          }
+          if (dogBreedPrimary && !dogBreedSecondary && !dogBreedMixed) {
+            return "This cutiepie is a " + dogBreedPrimary;
+          }
+          if (!dogBreedPrimary && !dogBreedSecondary && !dogBreedMixed) {
+            return "";
+          }
+        }
+        
         $('#match-main-display').remove();
-        var matchDisplay = $("<div id='match-main-display' class='container has-text-centered'><img id='match-img' src=" + response.data.animals[i].photos[0].medium + "> <h1 id='match-name' class='title is-3 has-text-white sriracha'>" + response.data.animals[i].name + "</h1><h2 id='match-dog-breed' class='subtitle is-4 has-text-white sriracha'>" + dogBreed() + "</h2><p id='match-description' class='is-size-5 has-text-white mali'>" + description + "</p><button id='accept-match' class='button is-danger is-size-2 has-text-weight-bold sriracha'>Accept Match!!!</button><br/><button id='decline-match' class='button is-dark is-size-4 sriracha'>Decline Match</button></div>");
+        if (response.data.animals.length) {
+          var description = response.data.animals[i].description
+          if (!description) description = "";
+          var matchDisplay = $("<div id='match-main-display' class='container has-text-centered'><img id='match-img' src=" + response.data.animals[i].photos[0].medium + "> <h1 id='match-name' class='title is-3 has-text-white sriracha'>" + response.data.animals[i].name + "</h1><h2 id='match-dog-breed' class='subtitle is-4 has-text-white sriracha'>" + dogBreed() + "</h2><p id='match-description' class='is-size-5 has-text-white mali'>" + description + "</p><button id='accept-match' class='button is-danger is-size-2 has-text-weight-bold sriracha'>Accept Match!!!</button><br/><button id='decline-match' class='button is-dark is-size-4 sriracha'>Decline Match</button></div>");
+        }
+        else{ //ADRIANA, ADD YOUR CODE HERE TO POP UP MODAL
+          var matchDisplay = $("<div>")
+          matchDisplay.text("please try")
+        }
         $('#main-body').append(matchDisplay);
 
         var acceptBtnClicked;
@@ -249,30 +255,30 @@ function buildRequest(searchGender, goodWithChildren, coatType, dogSize, goodWit
             acceptedDogIds.push(dogId);
             localStorage.setItem("matches", JSON.stringify(acceptedDogIds))
           }
+          
         });
 
         i++;
-
       }
 
       displayMatch();
-      $(document).on('click', '#decline-match', function () { getGif("dog+sad")});
-      $(document).on('click', '#accept-match', function () { getGif("dog+happy")})
+      $(document).on('click', '#decline-match', function () { getGif("dog+sad") });
+      $(document).on('click', '#accept-match', function () { getGif("dog+happy") })
 
-      function getGif(term){
+      function getGif(term) {
         $('#accept-match').remove();
         $('#decline-match').remove();
         $('#match-main-display').append($("<h1 id='congrats' class='title is-1 has-text-white sriracha'>Your Match has been Saved</h1>"));
         setTimeout(displayMatch, 2000);
 
         $(".modal-content").html("")
-        
+
         //RUN GIPHY
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LLdCkhWcP8YLeTTJPLSVyeqLFaiZlHiB&limit=10&rating=g&q="+term+"&SameSite=Secure";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LLdCkhWcP8YLeTTJPLSVyeqLFaiZlHiB&limit=10&rating=g&q=" + term + "&SameSite=Secure";
 
         $(".modal").addClass("is-active");
-        console.log(term , queryURL)
+        console.log(term, queryURL)
         $.ajax({
           url: queryURL,
           method: "GET"
